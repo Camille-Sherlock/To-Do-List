50728a5591112b97e10a3457f6d0f3aef9d4f906
Camillesherlock <ke.liu1@thoughtworks.com>
2020-12-23 20:00:06 +0800

    [lk]:use classname in ButtonWrapper

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { MODE_NONE, MODE_CREATE, MODE_SEARCH } from "../services/mode";
 
+let classNames = require("classnames");
+
 export default function ButtonWrapper(props) {
   const { mode, changeMode } = props;
   const isCreateMode = () => mode === MODE_CREATE;
   const isSearchMode = () => mode === MODE_SEARCH;
-
+  let btnAddClass = classNames({
+    "button-add": true,
+    "button-add-selected": isCreateMode(),
+  });
+  let btnSearchSelect = classNames({
+    "button-search": true,
+    "button-search-selected": isSearchMode(),
+  });
   return (
     <div data-testid="operation-menu">
       <button
         data-testid="addBtn"
         title="Add New"
-        classname={"button add " + (isCreateMode() ? "selected" : "")}
+        classname={btnAddClass}
         onClick={() => changeMode(isCreateMode() ? MODE_NONE : MODE_CREATE)}
       />
       <button
         data-testid="searchBtn"
         title="Search"
-        classname={"button search " + (isSearchMode() ? "selected" : "")}
+        classname={btnSearchSelect}
         onClick={() => changeMode(isSearchMode() ? MODE_NONE : MODE_SEARCH)}
       />
     </div>
