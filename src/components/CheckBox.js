f82c4425bef3fc6a3180b63fec7f7fae7bbe3a26
lrliang <392425595@qq.com>
2020-12-23 20:41:15 +0800

    [LLR] refactor CheckBox Component

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,30 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class CheckBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checked: this.props.checked,
-    };
-  }
+const CheckBox = ({ checked, onChange }) => {
+  const [isChecked, setIsChecked] = useState(checked);
 
-  handleChange(e) {
-    const { checked } = e.target;
+  const handleChange = () => {
+    setIsChecked(!checked);
+    onChange(!checked);
+  };
 
-    this.setState({ checked });
-    this.props.onChange(checked);
-  }
-
-  render() {
-    return (
-      <input
-        data-testid="input"
-        type="checkbox"
-        checked={this.state.checked}
-        onChange={this.handleChange.bind(this)}
-      />
-    );
-  }
-}
+  return (
+    <input
+      data-testid="input"
+      type="checkbox"
+      checked={isChecked}
+      onChange={handleChange}
+    />
+  );
+};
 
 export default CheckBox;
